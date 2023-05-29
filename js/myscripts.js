30ff269163eb05a5756515d0d6273051a1228f30
Catherine Gu <cgu@ucsd.edu>
2023-05-28 17:09:47 -0700

    added lightboxing

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,11 +1,18 @@
-const imageContainer = document.querySelector('.image-container');
-const image = imageContainer.querySelector('img');
-const closeButton = imageContainer.querySelector('.close-button');
+// expandable-image
+//make image fake lightbox
 
-image.addEventListener('click', () => {
-  imageContainer.classList.add('enlarged');
-});
+var expandableImages = document.getElementsByClassName('expandable-image');
+var expandedImg = document.querySelector('#expandedImage img');
+var expandedImageContainer = document.getElementById('expandedImage');
+var closeButton = document.getElementById('closeButton');
 
-closeButton.addEventListener('click', () => {
-  imageContainer.classList.remove('enlarged');
+for (var i = 0; i < expandableImages.length; i++) {
+  expandableImages[i].addEventListener('click', function () {
+    expandedImg.src = this.src;
+    expandedImageContainer.style.display = 'block';
+  });
+}
+
+closeButton.addEventListener('click', function () {
+  expandedImageContainer.style.display = 'none';
 });
